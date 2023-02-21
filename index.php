a01a30833130329d62c74c5227d186463b667319
Desh Piyumantha <121693916+DeshPrograming@users.noreply.github.com>
2023-02-21 11:53:27 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hundle</title>
+
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="bootstrap.css">
+    <link rel="icon" href="assets/img/favicon-32x32.png" />
+    <!-- <link rel="stylesheet" href="font-awesome.css"> -->
+    <link rel="stylesheet" href="font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="col-12 back">
+            <div class=" mycontent">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="row">
+                            <div class="logo"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 col-lg-7 image">
+
+                    </div>
+                    <div class="col-12 col-lg-5 detail">
+
+                        <h1 class="h1">Build The Community<br>Your Fans Will Love.</h1>
+                        <p class="p">Hunddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as engage in genuine discussion</p>
+                        <div class="row">
+                            <button class="button col-8 offset-2 col-lg-3 offset-lg-0">Register</button>
+                        </div>
+
+                    </div>
+                    <div class="col-12">
+                        <div class="social">
+
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+
+                            <a href="#"><i class="fa fa-twitter"></i></a>
+
+                            <a href="#"><i class="fa fa-instagram"></i></a>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+
+    <script src="script.js"></script>
+    <script src="bootstrap.js"></script>
+
+</body>
+
+</html>
